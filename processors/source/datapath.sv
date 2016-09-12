4963ec37b6ddd9ada8c99c7c15d760f7bb3ba7e1
Vikram Manja <vmanja@purdue.edu>
2016-09-12 16:54:43 -0400

    committing for sanity

--- processors/source/datapath.sv
+++ processors/source/datapath.sv
@@ -16,10 +16,25 @@ module datapath (
   input logic CLK, nRST,
   datapath_cache_if.dp dpif
 );
+
+dpif
+ //   input   ihit, imemload, dhit, dmemload,
+ //   output  halt, imemREN, imemaddr, dmemREN, dmemWEN, datomic,
+ //            dmemstore, dmemaddr
+
   // import types
   import cpu_types_pkg::*;
 
   // pc init
   parameter PC_INIT = 0;
+
+ // interfaces
+ alu_if al();
+ register_file_if rfif();
+
+// Main
+
+alu ALU (al); // still need to assign the values 
+
 
 endmodule
